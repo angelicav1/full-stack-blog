89a4a893bc7251a52542ab196ee06ebf9dd9f843
Angelica Victoria <114718142+angelicav1@users.noreply.github.com>
2024-12-15 10:51:35 -0800

    updated .env public keys

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,15 +1,15 @@
 import { useState } from "react";
-import { IKImage } from 'imagekitio-react';
 import Image from "./Image";
 import { Link } from "react-router-dom";
+import { SignedIn, SignInButton, SignedOut, UserButton } from "@clerk/clerk-react";
 
 const Navbar = () => {
     const [open, setOpen] = useState(false);
   return (
     <div className='w-full h-16 md:h-20 flex items-center justify-between'>
       {/* LOGO */}
-      <Link to="/" className='flex items-center gap-4 text-2xl font-bold'>
-        <Image src="logo.png" alt="Wild Byte Logo" w={32} h={32} />
+      <Link to='/' className='flex items-center gap-4 text-2xl font-bold'>
+        <Image src='logo.png' alt='Wild Byte Logo' w={32} h={32} />
         <span>Wild Byte</span>
       </Link>
       {/* MOBILE MENU */}
@@ -27,28 +27,34 @@ const Navbar = () => {
             open ? '-right-0' : '-right-[100%]'
           }`}
         >
-          <a href='/'>Home</a>
-          <a href='/'>Trending</a>
-          <a href='/'>Most Popular</a>
-          <a href='/'>About</a>
-          <a href=''>
+          <Link to='/'>Home</Link>
+          <Link to='/'>Trending</Link>
+          <Link to='/'>Most Popular</Link>
+          <Link to='/'>About</Link>
+          <Link to='/'>
             <button className='py-2 px-4 rounded-3xl bg-tomthumb text-white'>
               Login 👋
             </button>
-          </a>
+          </Link>
+          <SignedOut>
+            <SignInButton />
+          </SignedOut>
+          <SignedIn>
+            <UserButton />
+          </SignedIn>
         </div>
       </div>
       {/* DESKTOP MENU */}
       <div className='hidden md:flex items-center gap-8 xl-'>
-        <a href='/'>Home</a>
-        <a href='/'>Trending</a>
-        <a href='/'>Most Popular</a>
-        <a href='/'>About</a>
-        <a href=''>
+        <Link to='/'>Home</Link>
+        <Link to='/'>Trending</Link>
+        <Link to='/'>Most Popular</Link>
+        <Link to='/'>About</Link>
+        <Link to='/'>
           <button className='py-2 px-4 rounded-3xl bg-nandor text-white'>
             Login 👋
           </button>
-        </a>
+        </Link>
       </div>
     </div>
   );
--- src/main.jsx
+++ src/main.jsx
@@ -12,6 +12,13 @@ import LoginPage from "./routes/LoginPage.jsx";
 import RegisterPage from './routes/RegisterPage.jsx';
 import SinglePostPage from "./routes/SinglePostPage.jsx";
 import MainLayout from './layouts/MainLayout.jsx';
+import { ClerkProvider } from "@clerk/clerk-react";
+
+const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY;
+
+if (!PUBLISHABLE_KEY) {
+  throw new Error('Missing Publishable Key');
+}
 
 const router = createBrowserRouter([
   {element: <MainLayout />,
@@ -46,6 +53,8 @@ const router = createBrowserRouter([
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
+    <ClerkProvider publishableKey={PUBLISHABLE_KEY}>
     <RouterProvider router={router} />
+  </ClerkProvider>
   </StrictMode>
 );

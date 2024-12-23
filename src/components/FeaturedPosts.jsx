d4d2b08b57eeafb553dab5e8ddf4e1d6004d3833
Angelica Victoria <114718142+angelicav1@users.noreply.github.com>
2024-12-22 19:42:21 -0800

    created FeaturedPosts.jsx, PostListItem.jsx, and SinglePostPage.jsx

--- src/components/FeaturedPosts.jsx
+++ src/components/FeaturedPosts.jsx
@@ -7,7 +7,11 @@ const FeaturedPosts = () => {
         {/* First */}
         <div className='w-full lg:w-1/2 flex flex-col gap-4'>
           {/* image */}
-          <Image src='blog_pic_1.jpg' className='rounded-3xl object-cover' />
+          <Image
+            src='blog_pic_1.jpg'
+            className='rounded-3xl object-cover'
+            w='895'
+          />
           {/* details */}
           <div className='flex items-center gap-4'>
             <h1 className='font-semibold lg:text-lg'>01.</h1>
@@ -26,10 +30,13 @@ const FeaturedPosts = () => {
         <div className='w-full lg:w-1/2 flex flex-col gap-4'>
           {/* second */}
           <div className='lg:h-1/3 flex justify-between gap-4'>
-            <Image
-              src='blog_pic_2.jpg'
-              className='rounded-3xl object-cover w-1/3 aspect-video'
-            />
+            <div className='w-1/3 aspect-video'>
+              <Image
+                src='blog_pic_2.jpg'
+                className='rounded-3xl object-cover w-full h-full'
+                w='298'
+              />
+            </div>
             {/* details and title */}
             <div className='w-2/3'>
               {/* details */}
@@ -48,50 +55,54 @@ const FeaturedPosts = () => {
             </div>
           </div>
           {/* third */}
-            <div className='lg:h-1/3 flex justify-between gap-4'>
+          <div className='lg:h-1/3 flex justify-between gap-4'>
+            <div className='w-1/3 aspect-video'>
               <Image
                 src='blog_pic_3.jpg'
-                className='rounded-3xl object-cover w-1/3 aspect-video'
+                className='rounded-3xl object-cover w-full h-full' w="298"
               />
-              {/* details and title */}
-              <div className='w-2/3'>
-                {/* details */}
-                <div className='flex items-center gap-4 text-sm lg:text-base mb-4'>
-                  <h1 className='font-semibold'>03.</h1>
-                  <Link className='text-nandor'>Oceania</Link>
-                  <span className='text-gray-500 text-sm'>2 days ago</span>
-                </div>
-                {/* title */}
-                <Link
-                  to='/test'
-                  className='text-base sm:text-lg md:text-2xl lg:text-xl xl:text-2xl font-medium'
-                >
-                  Lorem ipsum dolor sit amet consectetur, adipisicing elit.
-                </Link>
+            </div>
+            {/* details and title */}
+            <div className='w-2/3'>
+              {/* details */}
+              <div className='flex items-center gap-4 text-sm lg:text-base mb-4'>
+                <h1 className='font-semibold'>03.</h1>
+                <Link className='text-nandor'>Oceania</Link>
+                <span className='text-gray-500 text-sm'>2 days ago</span>
               </div>
+              {/* title */}
+              <Link
+                to='/test'
+                className='text-base sm:text-lg md:text-2xl lg:text-xl xl:text-2xl font-medium'
+              >
+                Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+              </Link>
+            </div>
           </div>
           {/* fourth */}
-            <div className='lg:h-1/3 flex justify-between gap-4'>
-              <Image
-                src='blog_pic_4.jpg'
-                className='rounded-3xl object-cover w-1/3 aspect-video'
-              />
-              {/* details and title */}
-              <div className='w-2/3'>
-                {/* details */}
-                <div className='flex items-center gap-4 text-sm lg:text-base mb-4'>
-                  <h1 className='font-semibold'>04.</h1>
-                  <Link className='text-nandor'>Antarctica</Link>
-                  <span className='text-gray-500 text-sm'>2 days ago</span>
-                </div>
-                {/* title */}
-                <Link
-                  to='/test'
-                  className='text-base sm:text-lg md:text-2xl lg:text-xl xl:text-2xl font-medium'
-                >
-                  Lorem ipsum dolor sit amet consectetur, adipisicing elit.
-                </Link>
+          <div className='lg:h-1/3 flex justify-between gap-4'>
+          <div className="w-1/3 aspect-video">
+            <Image
+              src='blog_pic_4.jpg'
+              className='rounded-3xl object-cover w-full h-full' w="298"
+            />
+            </div>
+            {/* details and title */}
+            <div className='w-2/3'>
+              {/* details */}
+              <div className='flex items-center gap-4 text-sm lg:text-base mb-4'>
+                <h1 className='font-semibold'>04.</h1>
+                <Link className='text-nandor'>Antarctica</Link>
+                <span className='text-gray-500 text-sm'>2 days ago</span>
               </div>
+              {/* title */}
+              <Link
+                to='/test'
+                className='text-base sm:text-lg md:text-2xl lg:text-xl xl:text-2xl font-medium'
+              >
+                Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+              </Link>
+            </div>
           </div>
         </div>
       </div>
--- src/routes/SinglePostPage.jsx
+++ src/routes/SinglePostPage.jsx
@@ -1,7 +1,30 @@
-const SinglePostPage = () => {
-    return (
-        <div className="">SinglePostPage</div>
-    )
-}
+import { Link } from "react-router-dom";
+import Image from "../components/Image"
 
-export default SinglePostPage
+const SinglePostPage = () => {
+  return (
+    <div className='flex flex-col gap-8'>
+      {/* detail */}
+      <div className='flex gap-8'>
+        <div className='lg:w-3/5 flex flex-col gap-8'>
+          <h1 className="text-xl md:text-3xl xl:text-4xl 2xl:text-5xl font-semibold">Lorem ipsum dolor sit amet consectetur adipisicing elit.</h1>
+          <div className='flex items-center gap-2 text-gray-400 text-sm'>
+            <span>Written by</span>
+            <Link className="text-nandor">Angelica Victoria</Link>
+            <span>on</span>
+            <Link className="text-nandor">North America</Link>
+            <span>2 days ago</span>
+          </div>
+          <p className="text-tomthumb font-medium">Lorem ipsum dolor sit amet consectetur adipisicing elit. Distinctio, illum adipisci praesentium nostrum earum totam voluptas veniam. Maxime earum labore aut quia dignissimos ea inventore at culpa, commodi facere ipsam.</p>
+        </div>
+        <div className='hidden lg:block w-2/5'>
+          <Image src='blog_pic_1.jpg' w="600" className="rounded-2xl" />
+        </div>
+      </div>
+      {/* content */}
+      <div className=''></div>
+    </div>
+  );
+};
+
+export default SinglePostPage;

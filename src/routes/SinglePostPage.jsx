10ec0a08dd861a9a1f15ec5fc2bd238923fcc80c
Angelica Victoria <114718142+angelicav1@users.noreply.github.com>
2024-12-24 15:46:49 -0800

    added Search.jsx

--- src/routes/SinglePostPage.jsx
+++ src/routes/SinglePostPage.jsx
@@ -1,6 +1,7 @@
 import { Link } from "react-router-dom";
 import Image from "../components/Image"
 import PostMenuActions from "../components/PostMenuActions";
+import Search from "../components/Search";
 
 const SinglePostPage = () => {
   return (
@@ -28,9 +29,59 @@ const SinglePostPage = () => {
         </div>
       </div>
       {/* content */}
-      <div className='flex flex-col md:flex-row gap-8'>
+      <div className='flex flex-col md:flex-row gap-12'>
         {/* text */}
         <div className='lg:text-lg flex flex-col gap-6 text-justify'>
+          <p>
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Accusamus
+            a odit eligendi! Hic cupiditate veniam ut facere praesentium
+            explicabo, quas, aliquid obcaecati ipsam sunt odit sequi, nisi
+            voluptas. Molestiae, ducimus! Lorem ipsum dolor sit amet,
+            consectetur adipisicing elit. Fugiat, commodi. Dolor neque fuga
+            corrupti modi voluptatem et officiis, sunt quia rerum? Placeat
+            labore provident alias, assumenda possimus consequatur laboriosam
+            quod?
+          </p>
+          <p>
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Accusamus
+            a odit eligendi! Hic cupiditate veniam ut facere praesentium
+            explicabo, quas, aliquid obcaecati ipsam sunt odit sequi, nisi
+            voluptas. Molestiae, ducimus! Lorem ipsum dolor sit amet,
+            consectetur adipisicing elit. Fugiat, commodi. Dolor neque fuga
+            corrupti modi voluptatem et officiis, sunt quia rerum? Placeat
+            labore provident alias, assumenda possimus consequatur laboriosam
+            quod?
+          </p>
+          <p>
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Accusamus
+            a odit eligendi! Hic cupiditate veniam ut facere praesentium
+            explicabo, quas, aliquid obcaecati ipsam sunt odit sequi, nisi
+            voluptas. Molestiae, ducimus! Lorem ipsum dolor sit amet,
+            consectetur adipisicing elit. Fugiat, commodi. Dolor neque fuga
+            corrupti modi voluptatem et officiis, sunt quia rerum? Placeat
+            labore provident alias, assumenda possimus consequatur laboriosam
+            quod?
+          </p>
+          <p>
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Accusamus
+            a odit eligendi! Hic cupiditate veniam ut facere praesentium
+            explicabo, quas, aliquid obcaecati ipsam sunt odit sequi, nisi
+            voluptas. Molestiae, ducimus! Lorem ipsum dolor sit amet,
+            consectetur adipisicing elit. Fugiat, commodi. Dolor neque fuga
+            corrupti modi voluptatem et officiis, sunt quia rerum? Placeat
+            labore provident alias, assumenda possimus consequatur laboriosam
+            quod?
+          </p>
+          <p>
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Accusamus
+            a odit eligendi! Hic cupiditate veniam ut facere praesentium
+            explicabo, quas, aliquid obcaecati ipsam sunt odit sequi, nisi
+            voluptas. Molestiae, ducimus! Lorem ipsum dolor sit amet,
+            consectetur adipisicing elit. Fugiat, commodi. Dolor neque fuga
+            corrupti modi voluptatem et officiis, sunt quia rerum? Placeat
+            labore provident alias, assumenda possimus consequatur laboriosam
+            quod?
+          </p>
           <p>
             Lorem ipsum dolor sit, amet consectetur adipisicing elit. Accusamus
             a odit eligendi! Hic cupiditate veniam ut facere praesentium
@@ -59,29 +110,58 @@ const SinglePostPage = () => {
         </div>
         {/* menu */}
         <div className='px-4 h-max sticky top-8'>
-          <h1>Author</h1>
-          <div className=''>
-            <Image
-              src='userImg.jpeg'
-              className='w-12 h-12 rounded-full object-cover'
-              w='48'
-              h='48'
-            />
-            <Link>Angelica Victoria</Link>
-            <p>Lorem ipsum dolor sit amet.</p>
+          <h1 className='mb-4 text-sm font-medium'>Author</h1>
+          <div className='flex flex-col gap-4'>
+            <div className='flex items-center gap-8'>
+              <Image
+                src='userImg.jpeg'
+                className='w-12 h-12 rounded-full object-cover'
+                w='48'
+                h='48'
+              />
+              <Link className="text-tomthumb">Angelica Victoria</Link>
+            </div>
+            <p className="text-sm text-sprout font-bold">Lorem ipsum dolor sit amet.</p>
             <div className='flex gap-2'>
               <Link>
-                <Image src='facebook.svg'/>
+                <Image src='facebook.svg' />
               </Link>
               <Link>
-                <Image src='instagram.svg'/>
+                <Image src='instagram.svg' />
               </Link>
-              <Link>
-                <Image src='twitter-x.svg'/>
+              <Link className='mt-1'>
+                <Image src='twitter-x.svg' />
               </Link>
             </div>
           </div>
-          <PostMenuActions/>
+          <PostMenuActions />
+          <h1 className='mt-8 mb-4 text-sm font-medium'>Categories</h1>
+          <div className='flex flex-col gap-2 text-sm'>
+            <Link className='underline'>All</Link>
+            <Link className='underline' to='/'>
+              Africa
+            </Link>
+            <Link className='underline' to='/'>
+              Antarctica
+            </Link>
+            <Link className='underline' to='/'>
+              Asia
+            </Link>
+            <Link className='underline' to='/'>
+              Europe
+            </Link>
+            <Link className='underline' to='/'>
+              North America
+            </Link>
+            <Link className='underline' to='/'>
+              South America
+            </Link>
+            <Link className='underline' to='/'>
+              Oceania
+            </Link>
+          </div>
+          <h1 className='mt-8 mb-4 text-sm font-medium'>Search</h1>
+          <Search />
         </div>
       </div>
     </div>

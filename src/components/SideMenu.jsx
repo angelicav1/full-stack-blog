7598f485c9471310bd479556911c4a05574542bc
Angelica Victoria <114718142+angelicav1@users.noreply.github.com>
2024-12-26 11:34:59 -0800

    updated SideMenu.jsx

--- src/components/SideMenu.jsx
+++ src/components/SideMenu.jsx
@@ -7,7 +7,43 @@ const SideMenu = () => {
         <h1 className='mb-4 text-sm font-medium'>Search</h1>
         <Search />
         <h1 className='mt-8 mb-4 text-sm font-medium'>Filter</h1>
-        <div className="flex flex-col gap-2 text-sm"></div>
+        <div className='flex flex-col gap-2 text-sm'></div>
+        <label htmlFor='' className='flex items-center gap-2 cursor-pointer'>
+          <input
+            type='radio'
+            name='sort'
+            value='newest'
+            className='appearance-none w-4 h-4 border-[1.5px] border-nandor rounded-sm cursor-pointer  checked:bg-sprout bg-white'
+          />
+          Newest
+        </label>
+        <label htmlFor='' className='flex items-center gap-2 cursor-pointer'>
+          <input
+            type='radio'
+            name='sort'
+            value='most-popular'
+            className='appearance-none w-4 h-4 border-[1.5px] border-nandor rounded-sm cursor-pointer  checked:bg-sprout bg-white'
+          />
+          Most Popular
+        </label>
+        <label htmlFor='' className='flex items-center gap-2 cursor-pointer'>
+          <input
+            type='radio'
+            name='sort'
+            value='trending'
+            className='appearance-none w-4 h-4 border-[1.5px] border-nandor rounded-sm cursor-pointer  checked:bg-sprout bg-white'
+          />
+          Trending
+        </label>
+        <label htmlFor='' className='flex items-center gap-2 cursor-pointer'>
+          <input
+            type='radio'
+            name='sort'
+            value='oldest'
+            className='appearance-none w-4 h-4 border-[1.5px] border-nandor rounded-sm cursor-pointer  checked:bg-sprout bg-white'
+          />
+          Oldest
+        </label>
         <h1 className='mt-8 mb-4 text-sm font-medium'>Categories</h1>
         <div className='flex flex-col gap-4 text-sm'>
           <Link className='underline' to='/posts'>
